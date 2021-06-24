795782d72391d7c36eb3610df5deccc991d21514
Soraya Benson <soraya.benson@gmail.com>
2021-06-24 11:10:36 -0700

    header and nav links created, fonts imported

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import styles from '../../styles/Header.module.css';
 
 const Header = () => {
     return (
-        <div>
-            <h1>header</h1>
-        </div>
+        <header className={styles.header}>
+            <h1>SORAYA BENSON</h1>
+            <nav>
+                <Link href='/'>
+                    home
+                </Link>
+                <Link href='/projects'>
+                    projects
+                </Link>
+                <Link href='/about'>
+                    about
+                </Link>
+                <Link href='/connect'>
+                    connect
+                </Link>
+                <Link href='/resume'>
+                    cv
+                </Link>
+            </nav>
+        </header>
     )
 }
 
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,5 @@
-import Layout from '../components/Layout'
-import '../styles/globals.css'
-
+import Layout from '../components/Layout';
+import '../styles/globals.css';
 
 function App({ Component, pageProps }) {
   return (
