8044d9d7cbf5753d6f7d71d96f6d68a11a6a4b6a
Soraya Benson <soraya.benson@gmail.com>
2021-06-24 18:48:44 -0700

    footer links added

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -1,12 +1,28 @@
- import React from 'react';
- import styles from '../../styles/Footer.module.css';
- 
- const Footer = () => {
-     return (
-         <footer className={styles.footer}>
-             <h3>footer</h3>
-         </footer>
-     )
- }
- 
- export default Footer;
+import styles from '../../styles/Footer.module.css';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import TwitterIcon from '@material-ui/icons/Twitter';
+import EmailIcon from '@material-ui/icons/Email';
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <nav>
+                <a href="https://www.linkedin.com/in/soraya-benson/" target="_blank">
+                    <LinkedInIcon />
+                </a>
+                <a href="https://github.com/sorayabenson" target="_blank">
+                    <GitHubIcon />
+                </a>
+                <a href="https://twitter.com/sorayamajd" target="_blank">
+                    <TwitterIcon />
+                </a>
+                <a href="mailto:soraya.benson@gmail.com" target="_blank">
+                    <EmailIcon />
+                </a>
+            </nav>
+        </footer>
+    )
+}
+
+export default Footer;
