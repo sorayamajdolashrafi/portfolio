ee4c3f371b2b8c5e13e06f4a1c89980f9738a671
Soraya Benson <soraya.benson@gmail.com>
2021-08-12 17:19:10 -0700

    pdf viewer changed to inline

--- components/resume/AdobePdf.js
+++ components/resume/AdobePdf.js
@@ -3,30 +3,30 @@ import ViewSDKClient from "./ViewSDKClient";
 
 class AdobePdf extends Component {
 
-    viewerConfig = {
-        // pdf configuration https://github.com/adobe/pdf-embed-api-samples/blob/master/More%20Samples/React%20Samples/src/samples/ViewerCustomization.js
+    // viewerConfig = {
+    //     // pdf configuration https://github.com/adobe/pdf-embed-api-samples/blob/master/More%20Samples/React%20Samples/src/samples/ViewerCustomization.js
 
-        showDownloadPDF: true,
+    //     showDownloadPDF: true,
 
-        showPrintPDF: true,
+    //     showPrintPDF: true,
 
-        showPageControls: true,
+    //     showPageControls: true,
 
-        dockPageControls: true,
+    //     dockPageControls: true,
 
-        defaultViewMode: "FIT_PAGE",
-    };
+    //     defaultViewMode: "FIT_PAGE",
+    // };
 
 
     componentDidMount() {
         const viewSDKClient = new ViewSDKClient();
         viewSDKClient.ready().then(() => {
             /* Invoke file preview */
-            viewSDKClient.previewFile("pdf-div", this.viewerConfig);
-            // viewSDKClient.previewFile("pdf-div", {
-            //     /* Pass the embed mode option here */
-            //     embedMode: "IN_LINE"
-            // });
+            // viewSDKClient.previewFile("pdf-div", this.viewerConfig);
+            viewSDKClient.previewFile("pdf-div", {
+                /* Pass the embed mode option here */
+                embedMode: "IN_LINE"
+            });
         });
     }
 
