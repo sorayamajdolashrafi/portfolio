2536d8835ae1ca9b8fc96eb2316b19e4c8903903
Soraya Benson <soraya.benson@gmail.com>
2023-10-08 12:00:28 -0700

    update welcome and bio copy

--- components/about/Tech.jsx
+++ components/about/Tech.jsx
@@ -1,7 +1,6 @@
-import PropTypes from 'prop-types';
 import styles from '../../styles/about.module.css';
 
-const Tech = props => {
+const Tech = () => {
     return (
         <section className={styles.tech}>
             <h2>tech:</h2>
@@ -12,11 +11,11 @@ const Tech = props => {
                 </li>
                 <li>
                     <h3>testing suites</h3>
-                    <p>Jest, Supertest, QUnit, React Testing Library</p>
+                    <p>Jest, React Testing Library, QUnit,<br/>Playwright, Saucelabs</p>
                 </li>
                 <li>
                     <h3>libraries + frameworks</h3>
-                    <p>React, Node, Express, Superagent, Webpack, Material-UI, Socket.IO, Next.js</p>
+                    <p>React, Next.js, Webpack, Material-UI, Socket.IO,<br/>Node, Express, Superagent</p>
                 </li>
                 <li>
                     <h3>databases</h3>
@@ -28,15 +27,11 @@ const Tech = props => {
                 </li>
                 <li>
                     <h3>dev tools</h3>
-                    <p>VScode, Github, Postman, PGadmin, Heroku, Netlify, Adobe CC</p>
+                    <p>VScode, Github, Postman, PGadmin, Heroku, Netlify, Vercel, Adobe CC</p>
                 </li>
             </ul>
         </section>
     )
-}
-
-Tech.propTypes = {
-
 };
 
 export default Tech;
--- components/home/Welcome.jsx
+++ components/home/Welcome.jsx
@@ -1,19 +1,17 @@
-import PropTypes from 'prop-types';
 import styles from '../../styles/index.module.css';
 
-const Welcome = props => {
+const Welcome = () => {
     return (
         <div className={styles.moonCradle}>
             <div className={styles.moon}>
-                <h2>hi.</h2>
-                <h2>software engineer here.</h2>
+                <h2>oh, hello you.</h2>
+                <h2>i'm soraya.</h2>
+                <h2>a frontend software engineer.</h2>
+                <h2 className={styles.ghost}>👻</h2>
+                <div className={styles.shadow}></div>
             </div>
         </div>
     )
-}
-
-Welcome.propTypes = {
-
 }
 
 export default Welcome;
