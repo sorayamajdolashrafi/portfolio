49e81397b84ab60fd5e1108af15a83b39eb98864
Soraya Majdolashrafi <74790499+sorayamajdolashrafi@users.noreply.github.com>
2024-02-26 17:10:59 -0800

    update header name

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
         <div className={styles.header}>
             <header>
                 <Link href='/'>
-                    <h1>SORAYA BENSON</h1>
+                    <h1>SORAYA MAJDOLASHRAFI</h1>
                 </Link>
             </header>
         <nav>
